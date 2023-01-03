dd72e5496d07ace27b6cf63ca439f17504d9df30
MichalRys1 <116114555+MichalRys1@users.noreply.github.com>
2023-01-03 02:29:22 +0100

    v1.4

--- funkcje.cpp
+++ funkcje.cpp
@@ -27,3 +27,40 @@ float f_liniowa(float a, float b)
 		}
 	}
 }
+
+float f_liniowa2(float a, float b,float c)
+{
+	
+	if(b!=0)
+	{
+		a=a/-b;
+		c=c/-b;
+		
+		if(a==0)
+		{
+			if(c==0)
+			{
+				cout<<"Ta funkcja ma nieskonczenie wiele miejsc zerowych"<<endl;	
+			}
+			else
+			{
+				cout<<"Ta funkcja nie ma miejsc zerowych"<<endl;
+			}
+		}
+		else
+		{
+			if(-c/a==0)
+			{
+				return 0;
+			}
+			else
+			{
+				return -c/a;
+			}	
+		}
+	}
+	else
+	{
+		return -c/a;
+	}
+}
--- main.cpp
+++ main.cpp
@@ -5,19 +5,61 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	
-	float a,b;
+	float a,b,e;
 	
-	cout<<"Podaj a do obliczenia funkcji liniowej"<<endl;
-	cin>>a;
-	cout<<"Podaj b do obliczenia funkcji liniowej"<<endl;
-	cin>>b;
-	if(a==0)
+	int c,d;
+	
+	cout<<"Witaj w programie do liczenia miejsc zerowych funkcji liniowych i kwadratowych"<<endl;
+	cout<<"Wybierz typ funkcji do obliczenia"<<endl;
+	cout<<"1 - Funkcja liniowa"<<endl;
+	cout<<"2 - Funkcja kwadratowa"<<endl;
+	cin>>c;
+	switch(c)
 	{
-		cout<<f_liniowa(a,b)<<endl;
-	}
-	else
-	{
-		cout<<"Miejsce zerowe funkcji o a="<<a<<" i b="<<b<<" jest rowne "<<f_liniowa(a,b)<<endl;
+		case 1:
+			cout<<"Funkcja liniowa"<<endl;
+			cout<<"Wybierz postac funkcji liniowej"<<endl;
+			cout<<"1 - y=ax+b"<<endl;
+			cout<<"2 - Ax+By+C=0"<<endl;
+			cin>>d;
+			switch(d)
+			{
+				case 1:
+					cout<<"Podaj a do obliczenia funkcji liniowej"<<endl;
+					cin>>a;
+					cout<<"Podaj b do obliczenia funkcji liniowej"<<endl;
+					cin>>b;
+					
+						if(a==0)
+						{
+							cout<<f_liniowa(a,b)<<endl;
+						}
+						else
+						{
+							cout<<"Miejsce zerowe funkcji o a="<<a<<" i b="<<b<<" jest rowne "<<f_liniowa(a,b)<<endl;
+						}
+					break;
+				case 2:
+					cout<<"Podaj a do obliczenia funkcji liniowej"<<endl;
+					cin>>a;
+					cout<<"Podaj b do obliczenia funkcji liniowej"<<endl;
+					cin>>b;
+					cout<<"Podaj c do obliczenia funkcji liniowej"<<endl;
+					cin>>e;
+					
+						if(a==0)
+						{
+							cout<<f_liniowa2(a,b,e)<<endl;
+						}
+						else
+						{
+							cout<<"Miejsce zerowe funkcji o a="<<a<<" b="<<b<<" i c="<<e<<" jest rowne "<<f_liniowa2(a,b,e)<<endl;
+						}
+					
+			}
+			break;
+		case 2:
+			cout<<"Funkcja kwadratowa"<<endl;
 	}
 	
 	return 0;
